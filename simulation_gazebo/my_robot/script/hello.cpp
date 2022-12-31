defa8ed76f21e66b948be9edccb1977635ee6f62
liupaladin <ljxleo@umich.edu>
2022-12-30 18:38:41 -0800

    Modified format

--- simulation_gazebo/my_robot/script/hello.cpp
+++ simulation_gazebo/my_robot/script/hello.cpp
@@ -4,14 +4,13 @@ namespace gazebo
 {
   class WorldPluginMyRobot : public WorldPlugin
   {
-    public: WorldPluginMyRobot() : WorldPlugin()
-            {
-              printf("Hello World!\n");
-            }
+    public: WorldPluginMyRobot() : WorldPlugin() {
+        printf("Hello World!\n");
+    }
 
-    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-            {
-            }
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf){
+    
+    }
   };
   GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
 }
