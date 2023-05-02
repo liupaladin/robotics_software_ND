f3f587f6b2cf0b74dd943f293a62b2045c465700
jingxuan.liu <ljxleo@umich.edu>
2023-05-02 06:32:42 +0000

    Added markers test node

---
+++ catkin_ws/src/home_service_robot/pick_objects/src/pick_objects_test.cpp
@@ -0,0 +1,66 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+ 
+// Define a client for to send goal requests to the move_base server through a SimpleActionClient
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "simple_navigation_goals");
+
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  // Wait 5 sec for move_base action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  move_base_msgs::MoveBaseGoal goal;
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+  
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = -2.5;
+  goal.target_pose.pose.position.y = 3.5;
+  goal.target_pose.pose.orientation.w = 1.0;
+
+  // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+  
+  // Wait an infinite time for the results
+  ac.waitForResult();
+  
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+    ROS_INFO("Hooray, the base moved to the pickup position");
+    ros::Duration(5.0).sleep();
+  } else {
+    ROS_INFO("The base failed to navigate to pickup position");
+    return 0;
+  }
+
+  // Set second goal
+  goal.target_pose.pose.position.x = -7.5;
+  goal.target_pose.pose.position.y = -3.7;
+  goal.target_pose.pose.orientation.w = 1.0;
+  // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+    ROS_INFO("Hooray, the base moved to the delivery position");
+  } else {
+    ROS_INFO("Oops, the base didn't manage to move to the delivery position");
+  }
+  ROS_INFO("Mission accomplished");
+  ros::Duration(5.0).sleep();  
+  return 0;
+}
--- catkin_ws/src/home_service_robot/scripts/add_markers.sh
+++ catkin_ws/src/home_service_robot/scripts/add_markers.sh
@@ -5,5 +5,5 @@ xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e " rosrun pick_objects pick_objects_test" &
+xterm -e " rosrun add_markers add_markers_test" &
 sleep infinity
